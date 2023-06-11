1ae6c9c25f766d89c2dfbb2bc4214edc5121b6bc
Jimmy Jurado <jurado140@gmail.com>
2023-06-11 16:58:29 -0400

    Adds spec test for active and inactive coupon sections. Adds instance variables and creates model methods. Missing model test.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -62,4 +62,12 @@ class Merchant < ApplicationRecord
   def disabled_items
     items.where(status: 0)
   end
+
+  def active_coupons
+    coupons.where(status: 1)
+  end
+
+  def inactive_coupons
+    coupons.where(status: 0)
+  end
 end
