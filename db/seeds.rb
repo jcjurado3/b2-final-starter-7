8e89118cddc6c00cb6f173ccb1d8278266fbb4f7
Jimmy Jurado <jurado140@gmail.com>
2023-06-14 09:06:23 -0400

    Adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,68 @@ Transaction.destroy_all
 InvoiceItem.destroy_all
 Item.destroy_all
 Invoice.destroy_all
+Coupon.destroy_all
 Merchant.destroy_all
 Customer.destroy_all
 Rake::Task["csv_load:all"].invoke
+
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      @merchant2 = Merchant.create!(name: 'Jewelry')
+
+      @coupon1 = Coupon.create!(name: "Summer BOGO", unique_code: "BOGO50", discount: 50, merchant_id: @merchant1.id, discount_type: "percent", status: 1 )
+      @coupon2 = Coupon.create!(name: "Everthing Must Go", unique_code: "BOGO35", discount: 35, merchant_id: @merchant1.id, discount_type: "percent", status: 0 )
+      @coupon3 = Coupon.create!(name: "July4th", unique_code: "4SALE", discount: 15, merchant_id: @merchant1.id, discount_type: "dollar", status: 1 )
+      @coupon4 = Coupon.create!(name: "Winter Sale", unique_code: "WINTER35", discount: 35, merchant_id: @merchant1.id, discount_type: "dollar", status: 1 )
+      @coupon5 = Coupon.create!(name: "Fall Sale", unique_code: "FALL10", discount: 10, merchant_id: @merchant1.id, discount_type: "percent", status: 0 )
+      @coupon6 = Coupon.create!(name: "XMAS Sale", unique_code: "XMAS35", discount: 25, merchant_id: @merchant1.id, discount_type: "percent", status: 0 )
+
+      @coupon7 = Coupon.create!(name: "Summer BOGO", unique_code: "BOGO", discount: 50, merchant_id: @merchant2.id, discount_type: "percent", status: 1 )
+      @coupon8 = Coupon.create!(name: "Everthing Must Go", unique_code: "GO35", discount: 35, merchant_id: @merchant2.id, discount_type: "percent", status: 0 )
+      @coupon9 = Coupon.create!(name: "July4th", unique_code: "JULYSALE", discount: 15, merchant_id: @merchant2.id, discount_type: "dollar", status: 1 )
+      @coupon10 = Coupon.create!(name: "Winter Sale", unique_code: "SALE35", discount: 35, merchant_id: @merchant2.id, discount_type: "dollar", status: 1 )
+      @coupon11 = Coupon.create!(name: "Fall Sale", unique_code: "FALLSALE10", discount: 10, merchant_id: @merchant2.id, discount_type: "percent", status: 0 )
+
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
+      @item_3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: @merchant1.id)
+      @item_4 = Item.create!(name: "Hair tie", description: "This holds up your hair", unit_price: 1, merchant_id: @merchant1.id)
+      @item_7 = Item.create!(name: "Scrunchie", description: "This holds up your hair but is bigger", unit_price: 3, merchant_id: @merchant1.id)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+
+      @item_5 = Item.create!(name: "Bracelet", description: "Wrist bling", unit_price: 200, merchant_id: @merchant2.id)
+      @item_6 = Item.create!(name: "Necklace", description: "Neck bling", unit_price: 300, merchant_id: @merchant2.id)
+
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @customer_2 = Customer.create!(first_name: 'Cecilia', last_name: 'Jones')
+      @customer_3 = Customer.create!(first_name: 'Mariah', last_name: 'Carrey')
+      @customer_4 = Customer.create!(first_name: 'Leigh Ann', last_name: 'Bron')
+      @customer_5 = Customer.create!(first_name: 'Sylvester', last_name: 'Nader')
+      @customer_6 = Customer.create!(first_name: 'Herber', last_name: 'Kuhn')
+
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, coupon_id: @coupon1.id)
+      @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2)
+      @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2, coupon_id: @coupon3.id)
+      @invoice_4 = Invoice.create!(customer_id: @customer_3.id, status: 2, coupon_id: @coupon4.id)
+      @invoice_5 = Invoice.create!(customer_id: @customer_4.id, status: 2, coupon_id: @coupon1.id)
+      @invoice_6 = Invoice.create!(customer_id: @customer_5.id, status: 2)
+      @invoice_7 = Invoice.create!(customer_id: @customer_6.id, status: 1)
+      @invoice_8 = Invoice.create!(customer_id: @customer_6.id, status: 2)
+
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 0, created_at: "2012-03-27 14:54:09")
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 0, created_at: "2012-03-29 14:54:09")
+      @ii_3 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_2.id, quantity: 2, unit_price: 8, status: 2, created_at: "2012-03-28 14:54:09")
+      @ii_4 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_3.id, quantity: 3, unit_price: 5, status: 1, created_at: "2012-03-30 14:54:09")
+      @ii_6 = InvoiceItem.create!(invoice_id: @invoice_5.id, item_id: @item_4.id, quantity: 1, unit_price: 1, status: 1, created_at: "2012-04-01 14:54:09")
+      @ii_7 = InvoiceItem.create!(invoice_id: @invoice_6.id, item_id: @item_7.id, quantity: 1, unit_price: 3, status: 1, created_at: "2012-04-02 14:54:09")
+      @ii_8 = InvoiceItem.create!(invoice_id: @invoice_7.id, item_id: @item_8.id, quantity: 1, unit_price: 5, status: 1, created_at: "2012-04-03 14:54:09")
+      @ii_9 = InvoiceItem.create!(invoice_id: @invoice_7.id, item_id: @item_4.id, quantity: 1, unit_price: 1, status: 1, created_at: "2012-04-04 14:54:09")
+      @ii_10 = InvoiceItem.create!(invoice_id: @invoice_8.id, item_id: @item_4.id, quantity: 1, unit_price: 1, status: 1, created_at: "2012-04-04 14:54:09")
+
+      @transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_1.id)
+      @transaction2 = Transaction.create!(credit_card_number: 230948, result: 1, invoice_id: @invoice_2.id)
+      @transaction3 = Transaction.create!(credit_card_number: 234092, result: 1, invoice_id: @invoice_3.id)
+      @transaction4 = Transaction.create!(credit_card_number: 230429, result: 1, invoice_id: @invoice_4.id)
+      @transaction5 = Transaction.create!(credit_card_number: 102938, result: 1, invoice_id: @invoice_5.id)
+      @transaction6 = Transaction.create!(credit_card_number: 879799, result: 0, invoice_id: @invoice_6.id)
+      @transaction7 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_7.id)
+      @transaction7 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_8.id)
